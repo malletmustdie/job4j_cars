5fc8014c1b214f03c45b11a466ca787c2b5a5f9e
Elias <mallet322@gmail.com>
2022-03-14 09:26:57 +0300

    Реализовать пользовательские фильтры для площадок машин [#4745 #274014] - fix

--- src/main/java/ru/job4j/cars/repository/impl/AdRepositoryImpl.java
+++ src/main/java/ru/job4j/cars/repository/impl/AdRepositoryImpl.java
@@ -1,22 +1,30 @@
 package ru.job4j.cars.repository.impl;
 
-import java.util.List;
-
 import ru.job4j.cars.model.Advertisement;
 import ru.job4j.cars.repository.AbstractRepository;
 import ru.job4j.cars.repository.AdRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class AdRepositoryImpl extends AbstractRepository implements AdRepository {
 
     @Override
     public List<Advertisement> findAdvertisementsForTheLastDay() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDate localDate = LocalDate.now();
+        String startOfDay = formatter.format(LocalDateTime.of(localDate, LocalTime.MIDNIGHT));
+        String endOfDay = formatter.format(LocalDateTime.of(localDate, LocalTime.MAX));
         return this.tx(
                 session -> session.createQuery(
-                        "from Advertisement ad "
-                                + "where date_trunc('day', ad.created) "
-                                + "<= date_trunc('day', current_date)"
-                                  )
-                                  .list()
+                                "from Advertisement ad "
+                                        + "where ad.created between :from and :to")
+                        .setParameter("from", startOfDay)
+                        .setParameter("to", endOfDay)
+                        .list()
         );
     }
 
